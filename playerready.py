bc00dc2b32ce51754618231630a350d1ff2fcdea
Jacob1Carter <jacob1carter@outlook.com>
2026-05-07 23:43:46 +0100

    Created shifts table

--- playerready.py
+++ playerready.py
@@ -1,5 +1,7 @@
 from flask import session, Blueprint, render_template, request, redirect
-from tools import login_required, get_db_connection
+from tools import login_required, get_db_connection, ordinal
+from datetime import datetime
+import calendar
 import os
 
 
@@ -49,3 +51,79 @@ def query():
     conn.close()
 
     return render_template("playerready/query.html", query=query, data=data, columns=columns)
+
+
+@login_required
+@playerready.route("/shifttable")
+def shifttable():
+    id = session.get("id")
+    month = request.args.get("month", type=int)
+    year = request.args.get("year", type=int)
+    now = datetime.now()
+    print(now)
+    if not month:
+        month = now.month
+    if not year:
+        year = now.year
+    
+    days_json = {}
+    num_days = calendar.monthrange(year, month)[1]
+    for day in range(1, num_days + 1):
+        date_obj = datetime(year, month, day)
+        date_str = date_obj.strftime("%Y-%m-%d")
+        day_str = f"{date_obj.strftime('%a')} {ordinal(day)}"
+
+        conn, cur = get_db_connection()
+
+        cur.execute(f"SELECT id, clock_in, clock_out, shift_type FROM playerready_{id} WHERE DATE(clock_in) = '{date_str}'")
+        data = cur.fetchall()
+
+        shifts = []
+        for row in data:
+            clock_in_dt = datetime.strptime(row["clock_in"], "%Y-%m-%d %H:%M:%S")
+            clock_in_str = clock_in_dt.strftime("%H:%M")
+
+            clock_out_dt = datetime.strptime(row["clock_out"], "%Y-%m-%d %H:%M:%S")
+            clock_out_str = clock_out_dt.strftime("%H:%M")
+
+            duration = clock_out_dt - clock_in_dt
+            total_seconds = int(duration.total_seconds())
+            hours = total_seconds // 3600
+            minutes = (total_seconds % 3600) // 60
+            duration_str = f"{hours:02}:{minutes:02}"
+
+            shifts.append({"id": row["id"], "clock_in": clock_in_str, "clock_out": clock_out_str, "duration": duration_str})
+    
+        days_json[date_str] = {"day": day_str, "shifts": shifts}
+    
+    return render_template("playerready/shifttable.html", days=days_json)
+
+
+@login_required
+@playerready.route("/log-shift", methods=["POST"])
+def log_shift():
+    id = session.get("id")
+    date = request.form.get("date")
+    clock_in = request.form.get("clock-in")
+    clock_out = request.form.get("clock-out")
+
+    clock_in_dt = datetime.strptime(
+    f"{date} {clock_in}",
+    "%Y-%m-%d %H:%M"
+    )
+
+    clock_out_dt = datetime.strptime(
+        f"{date} {clock_out}",
+        "%Y-%m-%d %H:%M"
+    )
+    clock_in_sql = clock_in_dt.strftime("%Y-%m-%d %H:%M:%S")
+    clock_out_sql = clock_out_dt.strftime("%Y-%m-%d %H:%M:%S")
+
+    conn, cur = get_db_connection()
+
+    cur.execute(f"INSERT INTO playerready_{id} (clock_in, clock_out, shift_type) VALUES ('{clock_in_sql}', '{clock_out_sql}', 'type_placeholder')")
+
+    conn.commit()
+    conn.close()
+
+    return redirect("/playerready/shifttable")
